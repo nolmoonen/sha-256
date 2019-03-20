1525514854c38d0841c8f04344c723880765687b
nolmoonen <nolmoonen@hotmail.nl>
2019-03-20 22:32:54 +0100

    overengineering > hardcoding

--- main.c
+++ main.c
@@ -128,54 +128,9 @@ int main() {
 }
 
 word8 hex_to_int(char c) {
-    switch (c) {
-        case '0':
-            return 0;
-        case '1':
-            return 1;
-        case '2':
-            return 2;
-        case '3':
-            return 3;
-        case '4':
-            return 4;
-        case '5':
-            return 5;
-        case '6':
-            return 6;
-        case '7':
-            return 7;
-        case '8':
-            return 8;
-        case '9':
-            return 9;
-        case 'A':
-            return 10;
-        case 'B':
-            return 11;
-        case 'C':
-            return 12;
-        case 'D':
-            return 13;
-        case 'E':
-            return 14;
-        case 'F':
-            return 15;
-        case 'a':
-            return 10;
-        case 'b':
-            return 11;
-        case 'c':
-            return 12;
-        case 'd':
-            return 13;
-        case 'e':
-            return 14;
-        case 'f':
-            return 15;
-    }
-
-    return 16;
+    if (c > 96) return (uint8_t) (c - 87);
+    if (c > 64) return (uint8_t) (c - 55);
+    return (uint8_t) (c - 48);
 }
 
 int string_to_hash(word32 *hash, char *s) {
